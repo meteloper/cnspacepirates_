5643c77c106f23c5fd0d194e12a53521306a5c39
meteloper <yusufmetesaracoglu@gmail.com>
2023-10-13 17:05:48 +0300

    UI1001 - Standard UI design

---
+++ Assets/Scripts/UI/UIPopupConfirm.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SpacePirates.UI
+{
+    public class UIPopupConfirm : MonoBehaviour
+    {
+        public TextMeshProUGUI TextQuestion;
+        public TextMeshProUGUI TextTitle;
+        public TextMeshProUGUI TextButtonYesCaption;
+        public TextMeshProUGUI TextButtonNoCaption;
+        public Button ButtonYes;
+        public Button ButtonNo;
+    }
+}
+
---
+++ Assets/Scripts/UI/UIPopupPrompt.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+namespace SpacePirates.UI
+{
+    public class UIPopupPrompt : MonoBehaviour
+    {
+        public TMP_InputField InputFielPrompt;
+        public TextMeshProUGUI TextMessage;
+        public TextMeshProUGUI TextTitle;
+        public TextMeshProUGUI TextButtonOKCaption;
+        public Button ButtonOK;
+        public Button ButtonCancel;
+
+    }
+
+}
