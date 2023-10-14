a0f5db3ceb89c8b0a68fca0536d3a02a3a867389
meteloper <yusufmetesaracoglu@gmail.com>
2023-10-14 21:52:11 +0300

    +UI1002 was completed
    + Some files arrangements
    + Main menu design was completed

---
+++ Assets/Scripts/UI/Panels/PnlLogin.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SpacePirates.UI
+{
+    public class PnlLogin : MonoBehaviour
+    {
+        public Button BtnPlayAsGuest;
+        public Button BtnLogin;
+    }
+}
+
---
+++ Assets/Scripts/UI/Panels/ScreenHeadQuarters.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpacePirates.UI
+{
+    public class ScreenHeadQuarters : MonoBehaviour
+    {
+      
+    }
+}
